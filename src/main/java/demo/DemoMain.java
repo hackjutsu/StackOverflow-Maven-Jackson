ecfdeac4d0c78e74d57b5902b99858b5647547b0
hackjutsu <cosmo.lepton@gmail.com>
2017-05-06 17:55:14 -0700

    Initial commit

---
+++ src/main/java/demo/DemoMain.java
@@ -0,0 +1,12 @@
+package demo;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println("End");
+    }
+}
